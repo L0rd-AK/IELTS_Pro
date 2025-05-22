c10c7f189f1e35ff05ddf88e67f805ffb8b1e4fc
l0rd-AK <aka883207@gmail.com>
2025-05-23 05:04:18 +0600

    firebase information leak solved

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = { 
-  apiKey: "AIzaSyDcTBmL1CRXPGj9Ym5P1SWYdshcnpLiwCU", 
-  authDomain: "ielts-pro-ca12f.firebaseapp.com", 
-  projectId: "ielts-pro-ca12f", 
-  storageBucket: "ielts-pro-ca12f.firebasestorage.app", 
-  messagingSenderId: "775936084952", 
-  appId: "1:775936084952:web:3e2cb67af7033d00a580ad" 
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
